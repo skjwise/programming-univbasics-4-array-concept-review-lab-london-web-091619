ff328e25c467bf901775bb885c6b0052e8752400
Sarah Wise <skjwise@hotmail.com>
2019-08-25 14:41:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,6 @@ def find_element_index(array, value_to_find)
   array.each_with_index do |item, index|
     if item == value_to_find
       return index
-    else
-      return nil
-  end
 end
 return nil
 end
